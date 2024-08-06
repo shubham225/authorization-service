76a8f96af2021ce91d52e86fa382bb212c942ce8
Shubham Shinde <shubhamshinde225@gmail.com>
2024-08-06 23:19:09 +0530

    Thymeleaf deepndency added

---
+++ src/main/java/com/userservice/authorization/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.userservice.authorization.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    String getIndex() {
+        return "index";
+    }
+}
