279cb94249dacd35852e1c89c58b26ba513159ba
Shubham Shinde <shubhamshinde225@gmail.com>
2025-10-19 11:59:28 +0530

    Make use of Validation API to validate request

--- src/main/java/com/userservice/authorization/model/dto/RoleDTO.java
+++ src/main/java/com/userservice/authorization/model/dto/RoleDTO.java
@@ -1,5 +1,6 @@
 package com.userservice.authorization.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Builder
 public class RoleDTO {
     private UUID id;
+    @NotBlank
     private String role;
     private String description;
 }
